ac9fe9da9ad56d7201259681bfc4703156786a34
Daniel Klein <othercriteria@gmail.com>
2015-03-27 18:04:55 -0400

    Map overlay works

--- hexruns.py
+++ hexruns.py
@@ -5,11 +5,16 @@ import os
 import gpxpy
 import matplotlib.pyplot as plt
 import numpy as np
+import io
+from PIL import Image
+import urllib.request
 
 # Command line option handling
 parser = argparse.ArgumentParser(description = 'Visualize GPX running data.')
 parser.add_argument('locality', type = str, nargs = '?',
                     help = 'Limit visualization to selected locality')
+parser.add_argument('-dim', metavar = 'd', type = int, default = 600,
+                    help = 'Dimensions of image to output (default: 600)')
 args = parser.parse_args()
 
 if args.locality:
@@ -70,7 +75,37 @@ if args.locality:
     lat = lat[in_bounds]
     lon = lon[in_bounds]
     dur = dur[in_bounds]
-                
+    
+# Construct Google Maps static image request
+url = 'http://maps.googleapis.com/maps/api/staticmap'
+center_lat = np.mean(lat)
+center_lon = np.mean(lon)
+dim = args.dim
+range = max(lat.max() - lat.min(), lon.max() - lon.min())
+zoom = int(np.log(360 / range) / np.log(2) + 1)
+request = '{0}?center={1},{2}&size={3}x{3}&zoom={4}'.format(url,
+                                                            center_lat,
+                                                            center_lon,
+                                                            dim,
+                                                            zoom)
+
+# Make the request, grab the response, and prepare it for display
+opened_url = urllib.request.urlopen(request)
+contents = opened_url.read()
+buffer = io.BytesIO(contents)
+image = Image.open(buffer)
+
 plt.figure()
-plt.hexbin(lon, lat, dur, gridsize = 50)
+plt.imshow(image)
+
+# Transform the tracks onto the same coordinates as the map
+lat -= center_lat 
+lon -= center_lon
+scaling = (dim / 360) * (2 ** (zoom - 1))
+lat *= -scaling
+lon *= scaling
+lat += (dim / 2)
+lon += (dim / 2)
+
+plt.hexbin(lon, lat, dur, gridsize = 50, alpha = 0.4)
 plt.show()
