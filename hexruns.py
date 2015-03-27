78f5a8479a14fd1249775de1de7abed5be5be552
Daniel Klein <othercriteria@gmail.com>
2015-03-27 14:22:50 -0400

    Filtering by locality works

--- hexruns.py
+++ hexruns.py
@@ -4,16 +4,36 @@ import argparse
 import os
 import gpxpy
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Command line option handling
 parser = argparse.ArgumentParser(description = 'Visualize GPX running data.')
-parser.add_argument('city', type = str, nargs = '?',
-                    help = 'Limit visualization to selected city')
+parser.add_argument('locality', type = str, nargs = '?',
+                    help = 'Limit visualization to selected locality')
 args = parser.parse_args()
 
-if args.city:
-    print(args.city)
+if args.locality:
+    locality = args.locality
+    
+    print('Limiting to "{0}"'.format(locality))
 
+    api_key = open('google_public_api_key', 'r').readlines()[0][:-1]
+    print('Google public API key:', api_key)
+    
+    import googlemaps
+    gmaps = googlemaps.Client(key = api_key)
+
+    geocode_result = gmaps.geocode(locality)[0]['geometry']
+
+    bounds_l = geocode_result['bounds']['southwest']
+    bounds_u = geocode_result['bounds']['northeast']
+
+    lat_l, lat_u = bounds_l['lat'], bounds_u['lat']
+    lon_l, lon_u = bounds_l['lng'], bounds_u['lng']
+    
+    print('Latitude:  {0} -- {1}'.format(lat_l, lat_u))
+    print('Longitude: {0} -- {1}'.format(lon_l, lon_u))
+    
 # Accumulate lists of latitude, longitude, and duration
 lat, lon, dur = [], [], []
 
@@ -41,6 +61,16 @@ for file in all_files:
 
                 last = new
 
+lat = np.array(lat)
+lon = np.array(lon)
+dur = np.array(dur)
+if args.locality:
+    in_bounds = (lat_l < lat) * (lat < lat_u) * (lon_l < lon) * (lon < lon_u)
+
+    lat = lat[in_bounds]
+    lon = lon[in_bounds]
+    dur = dur[in_bounds]
+                
 plt.figure()
-plt.hexbin(lat, lon, dur, gridsize = 50)
+plt.hexbin(lon, lat, dur, gridsize = 50)
 plt.show()
