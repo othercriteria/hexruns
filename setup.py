8933964a8e0e007c6c64d7dad6d94dd7e77f522c
Daniel Klein <othercriteria@gmail.com>
2016-01-01 23:09:59 -0500

    Adding a basic setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = 'hexruns',
+      version = '0.1.0',
+      description = 'Visualizing running data through aggregation with hexagonal binning',
+      author = 'Daniel Klein',
+      author_email = 'othercriteria@gmail.com',
+      url = 'https://github.com/othercriteria/hexruns',
+      scripts = ['hexruns.py'],
+      )
